47d577ad16822598ca8fdae447b36a46a7f37461
tomek401273 <tomek371240@gmail.com>
2019-05-12 16:07:09 +0200

    first-commit

---
+++ src/main/java/com/tgrajkowski/reportgenerator/ReportGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.tgrajkowski.reportgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportGeneratorApplication.class, args);
+    }
+
+}
