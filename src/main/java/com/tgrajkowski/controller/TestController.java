c1bf4e41d364cc91f87e614a5b5c8d599a0fade2
tomek401273 <tomek371240@gmail.com>
2019-05-28 21:34:23 +0200

    testint-refresh

--- src/main/java/com/tgrajkowski/controller/TestController.java
+++ src/main/java/com/tgrajkowski/controller/TestController.java
@@ -4,13 +4,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class TestController {
     @Value("${cloud-repository.own.value}")
     private String myString;
 
     @GetMapping(value = "/test")
-    public String tes(){
+    public String tes() throws UnknownHostException {
+        System.out.println("myString: "+myString);
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        System.out.println("IP Address:- " + inetAddress.getHostAddress());
+        System.out.println("Host Name:- " + inetAddress.getHostName());
         return myString;
     }
 }
