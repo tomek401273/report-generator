3bed572765ad16fdcec949bcfb76f863225ec936
Tomasz Grajkowski <tgrajkowski@vigo.com.pl>
2019-06-14 09:59:06 +0200

    simplyfy

--- src/main/java/com/tgrajkowski/ReportGeneratorApplication.java
+++ src/main/java/com/tgrajkowski/ReportGeneratorApplication.java
@@ -1,30 +1,14 @@
 package com.tgrajkowski;
 
-import brave.sampler.Sampler;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
-@EnableFeignClients("com.tgrajkowski")
-@EnableDiscoveryClient
+
 @SpringBootApplication
 public class ReportGeneratorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReportGeneratorApplication.class, args);
 
-    }
-
-//    @Bean
-//    public void initBean() {
-//        System.out.println("checking is it really working...: "+myString);
-//    }
-
-    @Bean
-    public Sampler defaultSampler() {
-        return Sampler.ALWAYS_SAMPLE;
     }
 }
